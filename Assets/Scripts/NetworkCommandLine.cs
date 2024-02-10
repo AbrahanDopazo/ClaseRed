b07fb9240b049b229bf6aff3151192da3aac3604
AbrahanDopazo <abrahandopazo@gmail.com>
2024-02-10 11:55:29 +0100

    clase4

---
+++ Assets/Scripts/NetworkCommandLine.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkCommandLine : MonoBehaviour
+{
+    private NetworkManager netManager;
+    public MyNetworkController controller;
+
+    void Start()
+    {
+        netManager = GetComponentInParent<NetworkManager>();
+
+        if (Application.isEditor) return;
+
+        var args = GetCommandlineArgs();
+
+        if (args.TryGetValue("-mode", out string mode))
+        {
+            switch (mode)
+            {
+                case "server":
+                    controller.StartAsServer();
+                    break;
+                case "host":
+                    controller.StartAsHost();
+                    break;
+                case "client":
+                    controller.StartAsClient();
+                    break;
+            }
+        }
+    }
+
+    private Dictionary<string, string> GetCommandlineArgs()
+    {
+        Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+        var args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length; ++i)
+        {
+            var arg = args[i].ToLower();
+            if (arg.StartsWith("-"))
+            {
+                var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+                value = (value?.StartsWith("-") ?? false) ? null : value;
+
+                argDictionary.Add(arg, value);
+            }
+        }
+        return argDictionary;
+    }
+}
